9c598fe37f86dd5064b8086c8908ddc60b9aff3b
Edgard3033 <ed233094@outlook.com>
2023-03-06 11:34:34 -0500

    Pixelate created rows

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your code here
+const table = document.getElementById("table");
+
+function makeRow() {
+  const row = document.createElement("tr");
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement("td");
+    row.appendChild(td);
+  }
+  table.appendChild(row);
+}
+
+makeRow();
+makeRow();
+
+const button = document.getElementById("add-row");
+button.addEventListener("click", makeRow);
